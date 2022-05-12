e3c28d4a1aa5c58531cef8621cc14b9218410479
elizapimentel <elizapimentel@hotmail.com>
2022-05-12 17:10:50 -0300

    subindo exercicios da semana

--- exercicio.js
+++ exercicio.js
@@ -1,52 +1,71 @@
 /*
-1. Precisamos calcular o valor da parcela de uma Lace de cabelo humano que a Byanka Nicoli decidiu comprar, para isso precisamos 
-descobrir o preço do produto e em seguida o número de parcelas desejadas, a partir das funções a seguir, utilize qualquer uma das
-ferramentas aprendidas nesta aula para resolver o código assíncrono e obter o seguinte retorno no console: 
-
+1. Precisamos calcular o valor da parcela de uma Lace de cabelo humano que a Byanka Nicoli decidiu comprar, para isso precisamos descobrir o preço do produto e em seguida o número de parcelas desejadas, a partir das funções a seguir, utilize qualquer uma das ferramentas aprendidas nesta aula para resolver o código assíncrono e obter o seguinte retorno no console: 
 `Sua Lace custa R$3900,00 e você pagará em 10x de R$390,00`
 */
 
 // função que simula busca num banco que retorna o preço do produto:
 
 function buscarPreco(produto) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (produto === "hormonios") {
-        return {
-          nome: "Hormônios",
-          preco: 99.00
-        }
+        return resolve({
+            nome: "Hormônios",
+            preco: 99.00
+          })
+          
       } else if (produto === "unhas gel") {
-        return {
+        return resolve ({
           nome: "Unhas em Gel",
           preco: 190.00
-        }
+        })
       } else if (produto === "lace"){
-        return {
+        return resolve({
           nome: "Lace",
           preco: 3900.00
-        }
+        })
       } else {
-        return "Produto não encontrado"
+        return reject("Produto não encontrado", reject)
       }
   }, 2000)
+
+  })
 }
-  
+
+
 // função que simula busca num banco que retorna o valor das parcelas:
+
+
+function calcularParcela(valor) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve(
+        valor / parcelasDesejadas
+        )
+      }, 2000)
+    })
+  }
   
-function calcularParcela(preco) {
+  async function parcelar(produto) {
+    try {
+      const item = await buscarPreco(produto) //usar dentro da func o param que será informado ao final
+      const parcelado = await calcularParcela(item.preco)
+      
+      return console.log(`Sua ${item.nome} custa ${item.preco} e você pagará em ${parcelasDesejadas}x de ${parcelado}`)
+    }
+    catch(erro) {
+      return console.error("Deu ruim: ", erro)
+      
+    }
+  }
   let parcelasDesejadas = 10
-  setTimeout(() => {
-    return preco * parcelasDesejadas
-  }, 2000)
-}
+  parcelar("lace")
   
 
 /*
 2. Resolva usando async/await: 
 Você quer saber quanto vai pagar em reais por um produto comprado nos EUA e para isso precisa consultar numa "API"
-de cotação para descobrir o valor do Dólar no momento da compra (você deve usar o valor do dólar comercial) e calcular
-o valor em Real, em seguida precisa consultar outra "API" que retorna o valor de dois juros que serão cobrados sob o 
-preço em Real e retornar o valor final 
+de cotação para descobrir o valor do Dólar no momento da compra (você deve usar o valor do dólar comercial) e calcular o valor em Real, em seguida precisa consultar outra "API" que retorna o valor de dois juros que serão cobrados sob o preço em Real e retornar o valor final 
 
 dados:
 `const precoEmDolar = 1270  //preço em dólar`
@@ -79,6 +98,17 @@ function buscarJurosImportacao() {
 }
 
 async function calcularValorEmReal(precoEmDolar) {
-  try {} 
-  catch (error) {}
+  try {
+    const dolar = await buscarPrecoDolar() //pegar apenas a func referente para colocar em var
+    const juros = await buscarJurosImportacao()
+    const conversorParaReal = precoEmDolar*dolar.comercial //usar a func já como var
+    const jurosTotal = (conversorParaReal*juros.juros1) + (conversorParaReal*juros.juros2)
+    const valorFinal = conversorParaReal + jurosTotal
+    console.log(`O preço final do seu produto é ${valorFinal}`)
+  } 
+  catch (error) {
+    console.error(error)
+  }
 }
+
+calcularValorEmReal(1270)
